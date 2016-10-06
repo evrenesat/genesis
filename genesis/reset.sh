375a20c1d1c034136fa72f1fbe613e34ad1b3ddd
Evren Esat Özkan <sleytr@gmail.com>
2016-10-06 08:30:26 +0300

    more granular fixture loading

--- genesis/reset.sh
+++ genesis/reset.sh
@@ -8,5 +8,10 @@ createdb -E utf8 -O genesis genesis
 echo "db created"
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata ./fixtures/*.json
+python manage.py loaddata ./fixtures/01*json
+python manage.py loaddata ./fixtures/03*json
+python load_csv.py
+echo "Analyze data loaded"
+python manage.py loaddata ./fixtures/05*json
+#python manage.py loaddata ./fixtures/*.json
 
