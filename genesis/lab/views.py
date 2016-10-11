3bd72c68e76dd916b78b1ddf3517f1df5c30602a
Evren Esat Özkan <sleytr@gmail.com>
2016-10-12 01:29:29 +0300

    barcode

--- genesis/lab/views.py
+++ genesis/lab/views.py
@@ -1,6 +1,13 @@
+import barcode
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("foo")
+
+
+def get_anlyse_barcode(request, anylse_id):
+    EAN = barcode.get_barcode_class('ean13')
+    ean = EAN(u'5901234123457')
+    return ean.raw()
